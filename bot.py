feac5412131469ffba4c20d00338850c889ce26b
Sherwin Chen <chencw@umich.edu>
2021-05-13 16:36:07 +0800

    test

--- bot.py
+++ bot.py
@@ -1,70 +1,30 @@
 import os
 import json
-from datetime import datetime
 from pathlib import Path
-from discord.utils import get
 from discord.ext import commands
-import arrow
 import discord
-from tree import tree
+import chart
+
 
 BOT = commands.Bot("/")
 
 
-def is_in_chart_format(filename):
-    splited = filename.split(".")
-
-    if len(splited) != 3 \
-        or splited[0] != "chart" \
-        or splited[2] != "txt":
-        return False
-    
-    return True
-
-
-def get_checkin_emded(description):
-    result = discord.Embed(
-        title="譜面上傳成功！",
-        description=description,
-        color=discord.Color.blue())
-    result.set_footer(text=str(datetime.now()))
-    return result
-
-
 @BOT.command(help="Check in a chart.")
 async def checkin(ctx, path):
-    if ctx.message.channel:
-        # todo: return message
-        pass
-    
-    attachments = ctx.message.attachments
+    await chart.checkin(ctx, path)
 
-    if len(attachments) > 1 or len(attachments) == 0:
-        pass
-        # todo: say it's not allowed
+    os.system("git add .")
+    os.system("git commit -m \"test\"")
+    os.system("git push")
 
-    chart_file = attachments[0]
 
-    if not is_in_chart_format(chart_file.filename):
-        pass
-        # todo: say it's not allowed
-    
-    content_bytes = await chart_file.read()
-    chart_text = content_bytes.decode("utf-8")
-
-    directory = Path(f"{path}")
-    directory.mkdir(parents=True, exist_ok=True)
-    path = Path(f"{path}/{chart_file.filename}")
-    path.write_text(chart_text)
-
-    embed = get_checkin_emded(
-        f"{ctx.author.name}上傳了{path}")
-
-    await ctx.send(embed=embed)
+@BOT.command()
+async def checkout(ctx, path):
+    await chart.checkout(ctx, path)
 
 
 @BOT.command(help="View the files")
-async def tree(ctx):
+async def tree(ctx, path=None):
     def list_files(startpath):
         result = ""
         for root, dirs, files in os.walk(startpath):
@@ -77,9 +37,17 @@ async def tree(ctx):
                     continue
                 result += f"{subindent}{f}\n"
         return result
+    
+    if path is None:
+        path = ""
 
-    print(list_files(f"{os.getcwd()}/charts"))
+    text = list_files(f"{os.getcwd()}/charts/{path}")
+    
+    if text == "":
+        await ctx.send("這個路徑沒有譜面！")
+        return
 
+    await ctx.send(f"```\n{text}\n```")
 
 @BOT.command(help="delete a file")
 async def delete(ctx, path):
---
+++ chart.py
@@ -0,0 +1,111 @@
+import discord
+from datetime import datetime
+from pathlib import Path
+
+
+def get_charts_path(path):
+    return f"charts/{path}"
+
+
+def is_in_chart_format(filename):
+    splited = filename.split(".")
+    if len(splited) != 3 \
+        or splited[0] != "chart" \
+        or splited[2] != "txt":
+        return False
+    return True
+
+
+def get_checkin_success_embed(description):
+    result = discord.Embed(
+        title="譜面上傳成功！",
+        description=description,
+        color=discord.Color.blue())
+    result.set_footer(text=str(datetime.now()))
+    return result
+
+
+def get_checkin_fail_embed(description):
+    result = discord.Embed(
+        title="譜面上傳失敗",
+        description=description,
+        color=discord.Color.red())
+    result.set_footer(text=str(datetime.now()))
+    return result
+
+
+async def checkin(ctx, path):
+    if ctx.message.channel:
+        # todo: return message
+        pass
+    
+    attachments = ctx.message.attachments
+
+    if len(attachments) == 0:
+        emb = get_checkin_fail_embed("請附加譜面！")
+        await ctx.send(embed=emb)
+        return
+
+    chart_file = attachments[0]
+
+    if not is_in_chart_format(chart_file.filename):
+        emb = get_checkin_fail_embed("譜面的名字要是chart.[名字].txt！")
+        await ctx.send(embed=emb)
+        return
+    
+    content_bytes = await chart_file.read()
+    chart_text = content_bytes.decode("utf-8")
+
+    directory = Path(get_charts_path(path))
+    directory.mkdir(parents=True, exist_ok=True)
+    path = Path(get_charts_path(f"{path}/{chart_file.filename}"))
+    path.write_text(chart_text)
+
+    success_embed = get_checkin_success_embed(
+        f"{ctx.author.name}上傳了{path}")
+
+    await ctx.send(embed=success_embed)
+
+
+def get_checkin_success_embed(description):
+    return discord.Embed(
+        title="取出成功！",
+        description=description,
+        color=discord.Color.blue())
+
+
+def get_checkin_fail_embed(description):
+    return discord.Embed(
+        title="取出失敗！",
+        description=description,
+        color=discord.Color.red())
+
+
+async def checkout(ctx, path):
+    target_path = Path(get_charts_path(path))
+
+    if not target_path.exists():
+        emb = get_checkin_fail_embed("路徑不存在！")
+        await ctx.send(embed=emb)
+        return
+
+    charts = []
+    names = ""
+
+    if target_path.is_file():
+        with open(target_path) as chart:
+            names += chart.name + "\n"
+            charts.append(discord.File(
+                chart,
+                filename=chart.name.replace(str(target_path), "")))
+    
+    if target_path.is_dir():
+        for c in target_path.glob("chart.*.txt"):
+            with open(c) as chart:
+                names += chart.name + "\n"
+                charts.append(discord.File(
+                    chart,
+                    filename=chart.name.replace(str(target_path), "")))
+
+    success_emb = get_checkin_success_embed(names)
+    await ctx.send(files=charts, embed=success_emb)
